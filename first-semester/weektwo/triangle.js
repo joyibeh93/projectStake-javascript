384cb865f3562615761efbb4c9202629ff3cb23e
JOYCE <joychinwenduibeh93@gmail.com>
2023-09-26 14:07:58 +0100

    solutions for chessboard,fizzBuzz and triangel

---
+++ first-semester/weektwo/triangle.js
@@ -0,0 +1,16 @@
+// Write a loop that makes seven calls to console.log to output the following triangle:
+
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+// #######
+
+
+let number = "#";
+while(number.length <=7){
+  console.log(number)
+  number +='#';
+}
